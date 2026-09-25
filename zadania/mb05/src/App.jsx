09218f45aa11992b44fb8b87b187ce98f966a219
zawadkaantoni <zawadkaantoni2007@gmail.com>
2026-09-25 20:56:10 +0200

    mb5: navbar z przyciskami

--- zadania/mb05/src/App.jsx
+++ zadania/mb05/src/App.jsx
@@ -4,9 +4,37 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container mt-4">
-        <h1>Galeria zdjęć</h1>
-      </div>
+      <header className="container py-4 py-lg-5">
+        <div className="row align-items-center g-3">
+          <div className="col-12 col-lg-8">
+            <h1 className="mb-2">Galeria zdjęć</h1>
+            <p className="lead text-body-secondary mb-0">
+              Zdjęcia z wypraw w góry, nad morze i po mieście. Wybierz kategorię,
+              żeby zawęzić widok albo powiększ zdjęcie, które Ci się spodoba.
+            </p>
+          </div>
+          <div className="col-12 col-lg-4">
+            <div className="d-flex flex-wrap gap-2 justify-content-lg-end">
+              <button
+                type="button"
+                className="btn btn-outline-secondary"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#panelFiltrow"
+              >
+                Filtry
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#dodajZdjecie"
+              >
+                Dodaj zdjęcie
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
   )
 }
